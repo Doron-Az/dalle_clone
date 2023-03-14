711b5e9e9680f028ba24e57fca20dc8dcbd6862e
Doron Azulay <86197721+Doron-Az@users.noreply.github.com>
2023-03-14 18:14:57 +0200

    Finished the frontend and now working on the back

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,49 +1,48 @@
-import React , { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { preview } from '../assets'
-import { getRandomPrompt } from '../utils'
-import { FormField, Loader } from '../components'
+import { preview } from "../assets";
+import { getRandomPrompt } from "../utils";
+import { FormField, Loader } from "../components";
 
 const CreatePost = () => {
-
   const navigate = useNavigate();
   const [form, setForm] = useState({
-    name: '',
-    prompt: '',
-    name: '',
+    name: "",
+    prompt: "",
+    name: "",
   });
 
-  const [generatingUmg, setGeneratingImg] = useState(false);
+  const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
-const handleSubmit = () =>{
+  const generateImage = () => {};
 
-}
+  const handleSubmit = () => {};
 
-const handleChange = (e) => {
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value }) 
+  };
 
-}
+  const handleSurpriseMe = () => {
 
-const handleSurpriseMe = () => {
-
-}
-
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt })
+  };
 
   return (
-    <section className='max-w7xl mx-auto'>
+    <section className="max-w7xl mx-auto">
       <div>
-        <h1 className='font-extrabold text-[#222328] text-[32px]'>
-          Create
-        </h1>
-        <p className='mt-2 text-[#666e75] text-[16px] max-w[500px]'>
-          Create imaginative and visually stunning images through DALL-E AI and share them with the community
+        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w[500px]">
+          Create imaginative and visually stunning images through DALL-E AI and
+          share them with the community
         </p>
       </div>
 
-      <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
-        <div className='flex flex-col gap-5'>
-          <FormField 
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+          <FormField
             labelName="Your name"
             type="text"
             name="name"
@@ -52,7 +51,7 @@ const handleSurpriseMe = () => {
             handleChange={handleChange}
           />
 
-          <FormField 
+          <FormField
             labelName="Promt"
             type="text"
             name="Promt"
@@ -60,14 +59,54 @@ const handleSurpriseMe = () => {
             value={form.prompt}
             handleChange={handleChange}
             isSurpriseMe
-            handleSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
           />
+
+          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+            {form.photo ? (
+              <img
+                src={form.photo}
+                alt={form.prompt}
+                className="w-full h-full object-contain"
+              />
+            ) : (
+              <img
+                src={preview}
+                alt="preview"
+                className="w-9/12 h-9/12 object-contain opacity-40"
+              />
+            )}
+            {generatingImg && (
+              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+                <Loader />
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="mt-5 flex gap-5">
+          <button
+            type="button"
+            onClick={generateImage}
+            className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5"
+          >
+            {generatingImg ? "Generating..." : "Generate"}
+          </button>
+        </div>
+        <div className="mt-10">
+          <p className="mt-2 text-[#666e75] text-[14px]">
+            Once you have reated the image you want, you can share it with others in the community</p>
+            <button
+            type="submit"
+            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+
+            >
+            {loading ? "Sharing..." : "Share with the community"}
+            </button>
 
         </div>
       </form>
     </section>
+  );
+};
 
-  )
-}
-
-export default CreatePost
+export default CreatePost;
