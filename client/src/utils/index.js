b5943012fba29aac410d37ba087792dd73d9d514
Doron Azulay <86197721+Doron-Az@users.noreply.github.com>
2023-03-14 15:55:02 +0200

    some changes

---
+++ client/src/utils/index.js
@@ -0,0 +1,10 @@
+import { surpriseMePrompts } from '../constants'
+
+export function getRandomPrompt(prompt){
+    const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+    const randomPromt = surpriseMePrompts[randomIndex];
+    
+    if(randomPromt === prompt) return getRandomPrompt(prompt);
+
+    return randomPromt;
+}
