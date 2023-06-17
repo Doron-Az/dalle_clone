238de6221aab7e6fe90193d04bebd19faf6f571b
Doron Azulay <86197721+Doron-Az@users.noreply.github.com>
2023-06-17 22:07:48 +0300

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 import { Loader, Card, FormField } from "../components";
-
+//
 const RenderCards = ({ data, title }) => {
   if (data?.length > 0) {
     return data.map((post) => <Card key={post._id} {...post} />);
